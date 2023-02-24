3fd14d7dceb34a7bdd8ba0170aac25e81430ad87
g1Kwon <spring_shot@naver.com>
2023-02-24 16:25:40 +0900

    Tx Propagation example- REQUIRED, REQUIRES_NEW

--- src/main/java/hello/springtx/propagation/MemberService.java
+++ src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,8 +3,8 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Slf4j
 @Service
